b15683c6774ecdc19493e614d429f6fd2e9ea206
jfalxa <jfalxa.sta@gmail.com>
2017-07-24 23:35:58 +0200

    Order user posts chronologically

--- src/graphcool/post/userPosts.js
+++ src/graphcool/post/userPosts.js
@@ -5,7 +5,7 @@ export const userPosts = gql`query userPosts( $name: String! )
 {
     User(name: $name)
     {
-        posts
+        posts( orderBy: createdAt_DESC )
         {
             id
             title
