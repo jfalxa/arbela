11950efdd95e08bef4c4a9a5b2abcc9536e06b4e
jfalxa <jfalxa.sta@gmail.com>
2017-08-04 01:26:27 +0200

    Remove buggy header from UserLinks

--- src/components/links/UserLinks.js
+++ src/components/links/UserLinks.js
@@ -11,13 +11,11 @@ class UserLinks extends React.Component
 {
     render()
     {
-        const { loadingLinks, links, user } = this.props;
+        const { loadingLinks, links } = this.props;
 
         return (
 
             <section>
-
-                <h2>{ user && user.name }</h2>
 
                 { links && <LinkList items={ links } /> }
                 { loadingLinks && <p>Loading...</p> }
