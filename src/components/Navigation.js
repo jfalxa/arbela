8558d2f0a489d0da6b5f08629bb2f8449777cedb
jfalxa <jfalxa.sta@gmail.com>
2017-08-12 20:03:54 +0200

    Show the topbar links in one row

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,10 @@
 import React    from 'react';
 import { Link } from 'react-router-dom';
+import Box      from './generic/Box';
 import UserNav  from './auth/UserNav';
+
+
+const Ul = Box.withComponent( 'ul' );
 
 
 function Navigation( props )
@@ -8,13 +12,13 @@ function Navigation( props )
     return (
 
         <nav>
-            <ul>
+            <Ul row justifySpaceAround>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/new-link">Post link</Link></li>
-                <li><Link to="/new-board">Create board</Link></li>
                 <li><Link to="/boards">Boards</Link></li>
+                <li><Link to="/new-board">Create board</Link></li>
+                <li><Link to="/new-link">Post link</Link></li>
                 <li><UserNav /></li>
-            </ul>
+            </Ul>
         </nav>
 
     );
